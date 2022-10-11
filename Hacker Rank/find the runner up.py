31bd99cf6b514a949ddabd29a30d6325b37ea85f
MR G <50374607+ahmed-gouda@users.noreply.github.com>
2022-10-11 13:31:45 +0200

    Delete Hacker Rank directory

--- Hacker Rank/find the runner up.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    a=list(arr)
-    a.sort(reverse=True)
-    for i in range(n):
-        if(a[i]<a[0]):
-            print(a[i])
-            break
